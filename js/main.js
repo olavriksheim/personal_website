dfa2d78cb390e5feab31ea056de65e7543ab7a1f
Olav Elias Riksheim <olavelias@gmail.com>
2019-01-08 21:07:10 +0100

    Updates

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
-const menuBranding = document.querySelector('.menu-branding');
+const menuportrait = document.querySelector('.menu-portrait');
 const navItems = document.querySelectorAll('.nav-item');
 
 // Set Initial State Of Menu
@@ -16,7 +16,7 @@ function toggleMenu() {
     menuBtn.classList.add('close');
     menu.classList.add('show');
     menuNav.classList.add('show');
-    menuBranding.classList.add('show');
+    menuportrait.classList.add('show');
     navItems.forEach(item => item.classList.add('show'));
 
     showMenu = true;
@@ -24,7 +24,7 @@ function toggleMenu() {
     menuBtn.classList.remove('close');
     menu.classList.remove('show');
     menuNav.classList.remove('show');
-    menuBranding.classList.remove('show');
+    menuportrait.classList.remove('show');
     navItems.forEach(item => item.classList.remove('show'));
 
     showMenu = false;
